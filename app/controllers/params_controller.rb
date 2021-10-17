5cfbd546f1f04038621640b531a61000d5314469
Ian S <iansilber32@gmail.com>
2021-10-17 19:07:38 -0400

    adds both get query and get segment requests as well as post request

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,11 @@
 class ParamsController < ApplicationController
+  def scream
+    user_input = params[:word]
+    render json: user_input.upcase
+  end
+  def post_scream
+    word = params["word"]
+    word = word.upcase
+    render json: word
+  end
 end
